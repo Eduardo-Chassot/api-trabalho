03b855b0d028a23cb0a99f252d85134775c32078
Eduardo Chassot <dudunaboa@outlook.com>
2022-09-29 20:03:06 -0300

    [new] commiut

--- index.php
+++ index.php
@@ -33,7 +33,8 @@ require_once 'config.php';
         <div class='grafico'>
             <canvas id="myChart"></canvas>
         </div>
-        <div class="texto">Muitos dos jovens de hoje sofrem ou sofreram de depressão, não à toa é considerada o mal do século, em decorrência a está doença, ocorrem diversos casos de suicídio
+        <div class="texto">
+             Muitos dos jovens de hoje sofrem ou sofreram de depressão, não à toa é considerada o mal do século, em decorrência a está doença, ocorrem diversos casos de suicídio
              ao redor de todo o país. Ver um jovem tirar sua própria vida nos faz pensar as causas de sua ação e se o problema é individual ou de toda a sociedade. 
              Por meio de um sistema feito por nós, com a ajuda dos dados públicos disponibilizados pelo Atlas da Violência do Instituto de Pesquisa Econômica Avançada (IPEA), 
              iremos contabilizar, comparar e estudar os números registrados por cada estado, verificando se a quantidade de suicídios está relacionada com a qualidade de vida de 
@@ -42,34 +43,11 @@ require_once 'config.php';
              consideravelmente baixo possuem menos casos de suicídios comparados a estados com IDH elevado, como por exemplo, o Distrito Federal, dono do maior IDH do Brasil e com 
              população de aproximadamente 3 Milhões de pessoas, possui praticamente a mesma quantidade de casos do Piauí, que tem o 3º pior IDH Brasileiro e a população, também em 
              torno dos 3 Milhões. E em outros casos é justamente o contrário. Portanto, descobrimos que a depressão não escolhe alvos, todos podem ser afetados, indiferente de renda 
-             ou localidade.</div>
-        <a>1° Distrito Federal: 0,874.
-            2° Santa Catarina: 0,840.
-            3° São Paulo: 0,833.
-            4° Rio de Janeiro: 0,832.
-            5° Rio Grande do Sul: 0,832.
-            6° Paraná: 0,820.
-            7° Espírito Santo: 0,802.
-            8° Mato Grosso do Sul: 0,802.
-            9° Goiás: 0,800.
-            10° Minas Gerais: 0,800.
-            11° Mato Grosso: 0,796.
-            12° Amapá: 0,780.
-            13° Amazonas: 0,780.
-            14° Rondônia: 0,756.
-            15° Tocantins: 0,756.
-            16° Pará: 0,755.
-            17° Acre: 0,751.
-            18° Roraima: 0,750.
-            19° Bahia: 0,742.
-            20° Sergipe: 0,742.
-            21° Rio Grande do Norte: 0,738.
-            22° Ceará: 0,723.
-            23° Pernambuco: 0,718.
-            24° Paraíba: 0,718.
-            25° Piauí: 0,703.
-            26° Maranhão: 0,683.
-            27° Alagoas: 0,677.</a>
+             ou localidade.
+        </div>
+        <div class='grafico'>
+            <canvas id="ChartIDH"></canvas>
+        </div>
         <div class='linha'>
             <hr size="10" width="33%" noshade color='black'>
         </div>
--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 let verificador = 0;
 $(function(){
+    let chartStatus = Chart.getChart("myChart"); // <canvas> id
+    if (chartStatus != undefined) {
+         chartStatus.destroy();
+    }
+    let chartStatus2 = Chart.getChart("myChart"); // <canvas> id
+    if (chartStatus2 != undefined) {
+        chartStatus2.destroy();
+    }
     var estado = document.getElementById('selectEstado');
         estado.addEventListener("change", function(event) {
             let chartStatus = Chart.getChart("myChart"); // <canvas> id
             if (chartStatus != undefined) {
                 chartStatus.destroy();
+            }
+            let chartStatus2 = Chart.getChart("myChart"); // <canvas> id
+            if (chartStatus2 != undefined) {
+                chartStatus2.destroy();
             }
             console.log('entrou no js');
             var opcaoEstado = estado.options[estado.selectedIndex].value;
@@ -58,7 +70,35 @@ $(function(){
                         }
                     }
                 }
-         });
-        
-    })
+            });
+            
+        })
+        const ctx2 = document.getElementById('ChartIDH').getContext('2d');
+        const IDH = new Chart(ctx2, {
+            type: 'line',
+            data: {
+                    labels: ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SP', 'SE', 'TO'],
+                    datasets: [{
+                        label: 'IDH',
+                        data: [0.751, 0.677, 0.780, 0.780, 0.742, 0.723, 0.874, 0.802, 0.800, 0.683, 0.800, 0.802, 0.755, 0.718, 0.718, 0.703, 0.802, 0.832, 0.738, 0.756, 0.750, 0.832, 0.840, 0.833, 0.742, 0.756],
+                        borderColor: [
+                            'rgba(0, 0, 0)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+            options: {
+                plugins: {
+                    legend: {
+                        display:true,
+                        position:'bottom'
+                    }
+                },  
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
 });
